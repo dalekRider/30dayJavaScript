5e61fd26280ca331d0c7d411bd43dd86dc71baf6
dalekRider <jmercier76@gmail.com>
2017-12-18 16:57:04 -0500

    Update

--- 21-Geolocation/assets/js/geolocation.js
+++ 21-Geolocation/assets/js/geolocation.js
@@ -1,13 +1,18 @@
 
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
+const long = document.querySelector('.long-value');
+const lat = document.querySelector('.lat-value');
 
 //Watch version get position watch gives update based on change
-navigator.geolocation.watchPosition((data) => {
-  //console.log(data);
+navigator.geolocation.getCurrentPosition((data) => {
+  console.log(data);
+  console.log(data.coords.speed);
   speed.textContent = data.coords.speed;
   arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  long.textContent = data.coords.longitude;
+  lat.textContent = data.coords.latitude;
 }, (err) => {
   console.err(err);
-  alert('Please turn on your mobile device\'s location');
+  alert('Please turn on your location');
 });
