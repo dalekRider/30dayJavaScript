c79b2a574f085dc3061ffcf95e8cf2784ab92f99
dalekRider <jmercier76@gmail.com>
2017-12-18 16:08:04 -0500

    Daily and Minor Updates
    Day 21 Complete
    H1 tag updates

---
+++ 19-WebcamFun/assets/js/webcamFun.js
@@ -0,0 +1,105 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err =>{
+      console.error(`OHH NO!!!`, err);
+    });
+}
+
+function paintToCanvas () {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  // assign w/h to video w/h
+  canvas.width = width;
+  canvas.height = height;
+
+  //every x milliseconds take image from video and put intp canvas
+  return setInterval(()=>{
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //mess with them
+    //pixels = redEffect(pixels);
+    //pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
+    //put them back
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  //played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take data out of the paintToCanvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firsChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] + 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 100] = pixels.data[i + 1] - 50; // Green
+    pixels.data[i - 150] = pixels.data[i + 2] + 0.5; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  //get min/max green range
+  const levels = {};
+  //get all 6 selectors
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  console.log(levels);
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
---
+++ 21-Geolocation/assets/js/geolocation.js
@@ -0,0 +1,13 @@
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed');
+
+//Watch version get position watch gives update based on change
+navigator.geolocation.watchPosition((data) => {
+  console.log(data);
+  speed.textContent = data.coords.speed;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+  console.err(err);
+  alert('Please turn on your mobile device\'s location');
+});
