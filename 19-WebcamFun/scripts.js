05d7e3ad3a229ea7f3e9685c373b3ece40f9f111
Johnathan Mercier <jmercier76@gmail.com>
2018-06-12 22:01:43 -0400

    Comment corrections

--- 19-WebcamFun/scripts.js
+++ 19-WebcamFun/scripts.js
@@ -30,8 +30,8 @@ function paintToCanvas () {
     let pixels = ctx.getImageData(0, 0, width, height);
     //mess with them
     //pixels = redEffect(pixels);
-    //pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.1;
+    pixels = rgbSplit(pixels);
+    //ctx.globalAlpha = 0.1;
     //put them back
     pixels = greenScreen(pixels);
     ctx.putImageData(pixels, 0, 0);
