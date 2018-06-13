269a7c2400d0f2b7cab4608d37a2a2645f93419e
Johnathan Mercier <jmercier76@gmail.com>
2018-06-12 22:24:01 -0400

    comment correctiono

--- 19-WebcamFun/scripts.js
+++ 19-WebcamFun/scripts.js
@@ -78,7 +78,7 @@ function greenScreen(pixels) {
     levels[input.name] = input.value;
   });
 
-  console.log(levels);
+  //console.log(levels);
 
   for (i = 0; i < pixels.data.length; i = i + 4) {
     red = pixels.data[i + 0];
